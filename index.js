d5698db961005efe5167cfa381ccf47b269a503c
Sujinathakuri <sujchand2@gmail.com>
2021-05-03 10:04:06 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = CurrentUsers;
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
